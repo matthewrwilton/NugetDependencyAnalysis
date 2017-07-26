db316104ee1e1a3ce8b05c988fa1a28f7de1ea32
Matthew Wilton <matthewrwilton@gmail.com>
2017-07-26 23:06:55 +1000

    Basic code for finding packages.config files

--- NugetDependencyAnalysis/Program.cs
+++ NugetDependencyAnalysis/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,79 @@ namespace NugetDependencyAnalysis
     {
         static void Main(string[] args)
         {
+        }
+    }
+
+    internal class PackagesConfigFinder
+    {
+        public IReadOnlyList<PackagesConfig> Find(string directory)
+        {            
+            var packagesConfigs = new List<PackagesConfig>();
+
+            var root = new DirectoryInfo(directory);
+
+            return root.GetFiles("packages.config", SearchOption.AllDirectories)
+                .Select(packagesConfigFile => CreatePackagesConfig(packagesConfigFile))
+                .ToList();
+        }
+
+        private PackagesConfig CreatePackagesConfig(FileInfo packagesConfigFile)
+        {
+            var projectFiles = packagesConfigFile.Directory.GetFiles("*.csproj", SearchOption.TopDirectoryOnly)
+                .ToList();
+
+            // TODO: Better error handling.
+            if (projectFiles.Count == 0)
+            {
+                throw new Exception();
+            }
+            if (projectFiles.Count > 1)
+            {
+                throw new Exception();
+            }
+
+            var projectFile = projectFiles.Single();
+            var projectName = projectFile.Name.Substring(0, projectFile.Name.Length - projectFile.Extension.Length);
+
+            return new PackagesConfig(packagesConfigFile.FullName, projectName);
+        }
+    }
+
+    internal class PackagesConfig
+    {
+        public PackagesConfig(string path, string projectName)
+        {
+            Path = path;
+            ProjectName = projectName;
+        }
+
+        public string Path { get; }
+
+        public string ProjectName { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj) || obj.GetType() != GetType())
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return Equals((PackagesConfig)obj);
+        }
+
+        protected bool Equals(PackagesConfig other)
+        {
+            return Path == other.Path &&
+                ProjectName == other.ProjectName;
+        }
+
+        public override int GetHashCode()
+        {
+            throw new InvalidOperationException($"{nameof(PackagesConfig)} is not intended to be the key in a collection.");
         }
     }
 }
---
+++ NugetDependencyAnalysisTests/PackagesConfigFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NugetDependencyAnalysis;
+using Xunit;
+
+namespace NugetDependencyAnalysisTests
+{
+    public class PackagesConfigFinderTests
+    {
+        [Fact]
+        public void Finds_PackagesConfig_In_Directory()
+        {
+            // TODO: replace with function to find directory.
+            var testDirectory = @"C:\repos\github\matthewrwilton\NugetDependencyAnalysis\NugetDependencyAnalysisTests\_TestData";
+
+            var target = new PackagesConfigFinder();
+
+            var actual = target.Find(testDirectory);
+
+            var expected = new List<PackagesConfig> {
+                new PackagesConfig(@"C:\repos\github\matthewrwilton\NugetDependencyAnalysis\NugetDependencyAnalysisTests\_TestData\SampleSolution\SampleProject\packages.config", "SampleProject")
+            };
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}
