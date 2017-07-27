40d26b7e81f709332e3ed72dd8f35335118d108e
Matthew Wilton <matthewrwilton@gmail.com>
2017-07-27 23:07:39 +1000

    Handle missing and multiple project files

--- NugetDependencyAnalysis/Program.cs
+++ NugetDependencyAnalysis/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Serilog;
 
 namespace NugetDependencyAnalysis
 {
@@ -16,30 +15,52 @@ namespace NugetDependencyAnalysis
 
     internal class PackagesConfigFinder
     {
+        private const string PackagesConfigFileName = "packages.config";
+
+        public PackagesConfigFinder(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        private ILogger Logger { get; }
+
         public IReadOnlyList<PackagesConfig> Find(string directory)
         {            
             var packagesConfigs = new List<PackagesConfig>();
 
             var root = new DirectoryInfo(directory);
 
-            return root.GetFiles("packages.config", SearchOption.AllDirectories)
-                .Select(packagesConfigFile => CreatePackagesConfig(packagesConfigFile))
+            var packagesConfigFiles = root.GetFiles(PackagesConfigFileName, SearchOption.AllDirectories)
                 .ToList();
+
+            foreach (var packagesConfigFile in packagesConfigFiles)
+            {
+                var packagesConfig = CreatePackagesConfig(packagesConfigFile);
+                if (packagesConfig == null)
+                {
+                    continue;
+                }
+
+                packagesConfigs.Add(packagesConfig);
+            }
+
+            return packagesConfigs;
         }
 
         private PackagesConfig CreatePackagesConfig(FileInfo packagesConfigFile)
         {
             var projectFiles = packagesConfigFile.Directory.GetFiles("*.csproj", SearchOption.TopDirectoryOnly)
                 .ToList();
-
-            // TODO: Better error handling.
+            
             if (projectFiles.Count == 0)
             {
-                throw new Exception();
+                Logger.Warning($"Skipping {PackagesConfigFileName} in {{Directory}} because the project file is missing", packagesConfigFile.Directory.FullName);
+                return null;
             }
             if (projectFiles.Count > 1)
             {
-                throw new Exception();
+                Logger.Warning($"Skipping {PackagesConfigFileName} in {{Directory}} because there are multiple project files", packagesConfigFile.Directory.FullName);
+                return null;
             }
 
             var projectFile = projectFiles.Single();
--- NugetDependencyAnalysisTests/PackagesConfigFinderTests.cs
+++ NugetDependencyAnalysisTests/PackagesConfigFinderTests.cs
@@ -1,27 +1,58 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using FluentAssertions;
+using Moq;
 using NugetDependencyAnalysis;
+using Serilog;
 using Xunit;
 
 namespace NugetDependencyAnalysisTests
 {
     public class PackagesConfigFinderTests
     {
+        public PackagesConfigFinderTests()
+        {
+            var mockLogger = new Mock<ILogger>();
+            Target = new PackagesConfigFinder(mockLogger.Object);
+        }
+
+        private string TestDirectoriesLocation => Path.Combine(Environment.CurrentDirectory, "_TestData");
+
+        private PackagesConfigFinder Target { get; }
+
         [Fact]
         public void Finds_PackagesConfig_In_Directory()
         {
-            // TODO: replace with function to find directory.
-            var testDirectory = @"C:\repos\github\matthewrwilton\NugetDependencyAnalysis\NugetDependencyAnalysisTests\_TestData";
+            var testDirectory = Path.Combine(TestDirectoriesLocation, "SampleSolution");
 
-            var target = new PackagesConfigFinder();
-
-            var actual = target.Find(testDirectory);
+            var actual = Target.Find(testDirectory);
 
             var expected = new List<PackagesConfig> {
-                new PackagesConfig(@"C:\repos\github\matthewrwilton\NugetDependencyAnalysis\NugetDependencyAnalysisTests\_TestData\SampleSolution\SampleProject\packages.config", "SampleProject")
+                new PackagesConfig(Path.Combine(TestDirectoriesLocation, @"SampleSolution\SampleProject\packages.config"), "SampleProject")
             };
 
             actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Handles_Missing_Project_Files()
+        {
+            var testDirectory = Path.Combine(TestDirectoriesLocation, "NoProjectFile");
+
+            var actual = Target.Find(testDirectory);
+
+            actual.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Handles_Multiple_Project_Files()
+        {
+            var testDirectory = Path.Combine(TestDirectoriesLocation, "MultipleProjectFiles");
+
+            var actual = Target.Find(testDirectory);
+
+            actual.Should().BeEmpty();
         }
     }
 }
