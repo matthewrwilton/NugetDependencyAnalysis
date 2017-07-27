86cdd9a23144c210a95568fc9ffd155ee44923f9
Matthew Wilton <matthewrwilton@gmail.com>
2017-07-27 23:27:53 +1000

    Wire up PackagesConfigFinder instance in Program

--- NugetDependencyAnalysis/Program.cs
+++ NugetDependencyAnalysis/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace NugetDependencyAnalysis
+﻿using System;
+using Serilog;
+
+namespace NugetDependencyAnalysis
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("NugetDependencyAnalysis expects a single parameter, the directory to analyse in.");
+                Console.WriteLine();
+                Console.WriteLine("Usage: NugetDependencyAnalysis.exe <directory to analyse>");
+                Console.WriteLine("  E.g. NugetDependencyAnalysis.exe \"C:\\repos\"");
+                Console.WriteLine();
+                return;
+            }
+
+            var root = args[0];
+
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var packagesConfigFinder = new PackagesConfigFinder(logger);
+            var packagesConfigs = packagesConfigFinder.Find(root);
+
+
         }
     }
 }
